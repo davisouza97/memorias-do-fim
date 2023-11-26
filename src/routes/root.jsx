8eae0fe3334d719a2dd9254c8e975a56f48cbb23
Davi Souza <davisouza1997jf@gmail.com>
2023-11-26 18:24:58 -0300

    Update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -9,7 +9,7 @@ export default function Root() {
             </div>
 
             <map name="image-map">
-                <area href="area1" coords="178,430,59" shape="circle" />
+                <area href="#area1" coords="178,430,59" shape="circle" />
                 <area target="_parent" alt="b" title="b" href="b" coords="317,514,55" shape="circle" />
                 <area target="_self" alt="c" title="c" href="c" coords="428,641,52" shape="circle" />
                 <area target="_top" alt="d" title="d" href="d" coords="594,465,52" shape="circle" />
